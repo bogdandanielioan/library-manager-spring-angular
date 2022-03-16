fd46acfa423bf116ff1f325cc6ccbe1e3661258e
Bogdan Daniel Ioan <bogdan.daniel.ioan@outlook.com>
2022-03-16 16:16:47 +0200

    first commit

---
+++ src/main/java/ro/library/librarymanagerspringangular/LibraryManagerSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package ro.library.librarymanagerspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerSpringAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagerSpringAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/library/librarymanagerspringangular/LibraryManagerSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.library.librarymanagerspringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerSpringAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
